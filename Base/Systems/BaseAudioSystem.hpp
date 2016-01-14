4788dacabd3fe4c45e0a35e94debe453d38fce86
Marcelino Coll <coll.marce@gmail.com>
2016-01-14 19:44:21 +0000

    Created basic interface for audio systems

--- Base/Systems/BaseAudioSystem.hpp
+++ Base/Systems/BaseAudioSystem.hpp
@@ -2,6 +2,7 @@
 #define __ATOMHEART_BASEAUDIOSYSTEM__
 
 // LIB INCLUDES
+#include <SFML/Audio.hpp>
 
 // ENGINE INCLUDES
 #include "System.hpp"
@@ -10,6 +11,17 @@ class BaseAudioSystem : public System  {
 public:
   BaseAudioSystem();
   ~BaseAudioSystem();
+
+  virtual void PlaySound();
+  virtual void PlayMusic();
+  virtual void StopMusic();
+  virtual void PauseMusic();
+  virtual void SetLoop();
+  virtual void SetPitch();
+  virtual void SetVolume();
+  virtual void SetPosition();
+  virtual void SetGlobalSoundVolume();
+  virtual void SetGlobalMusicVolume();
 
 private:
 };
