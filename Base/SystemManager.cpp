68c74b70699a9d3313a7d876dae402dd4eafbccc
Marcelino Coll <coll.marce@gmail.com>
2016-01-14 19:43:50 +0000

    Added initialization of base systems on system manager creation

--- Base/SystemManager.cpp
+++ Base/SystemManager.cpp
@@ -2,9 +2,20 @@
 
 SystemManager::SystemManager() {
   systems = std::vector<System*>(sizeof(TypeId), nullptr);
+
+  InitializeBaseSystems();
 }
 
 SystemManager::~SystemManager() {}
+
+void SystemManager::InitializeBaseSystems() {
+  Register<BaseAudioSystem>();
+  Register<BaseSceneSystem>();
+  Register<BaseRenderSystem>();
+  Register<BaseInputSystem>();
+  Register<BaseDataSystem>();
+  Register<BaseStateSystem>();
+}
 
 void SystemManager::addSystem(System* sys, TypeId tid) {
   systems[tid] = sys;
--- Base/SystemManager.hpp
+++ Base/SystemManager.hpp
@@ -19,6 +19,8 @@ public:
   SystemManager();
   ~SystemManager();
 
+  void InitializeBaseSystems();
+
   template <typename T, typename... Args>
   System& Register(Args&&... args);
 
