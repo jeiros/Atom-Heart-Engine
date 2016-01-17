05ef12f96dc6dcc185fe02736924a5b61e5065c9
Marcelino Coll <coll.marce@gmail.com>
2016-01-17 21:49:26 +0000

    Cleanup

--- src/Base/Resources.cpp
+++
@@ -1,9 +0,0 @@
-#include "Resources.hpp"
- 
-// sf::Texture Resources::texture1;
-sf::Texture Resources::test_texture;
-
-void Resources::load() {
-  //texture1.loadFromFile("blah.png");
-  test_texture.loadFromFile("test.png");
-}
